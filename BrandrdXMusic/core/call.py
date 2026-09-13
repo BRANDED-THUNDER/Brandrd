b45f8afdc0f9b6d91a13b5ca48c6468c6dd1fd64
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-13 20:15:23 +0530

    Update call.py

--- BrandrdXMusic/core/call.py
+++ BrandrdXMusic/core/call.py
@@ -8,7 +8,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
 from ntgcalls import TelegramServerError
 from pytgcalls import PyTgCalls
-from pytgcalls import filters as pytgcalls_filters
 
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
