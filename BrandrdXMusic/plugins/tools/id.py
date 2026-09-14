8cbe6f687da7a9a9ca18d72a339ae83e2e2318ea
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 13:15:53 +0530

    Update id.py

--- BrandrdXMusic/plugins/tools/id.py
+++ BrandrdXMusic/plugins/tools/id.py
@@ -3,19 +3,47 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.command("id"))
-def ids(_, message):
-    reply = message.reply_to_message
-    if reply:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
-        markup = InlineKeyboardMarkup([[button]])
-        message.reply_text(
-            f"User {reply.from_user.first_name} ID is : {reply.from_user.id}",
-            reply_markup=markup
+async def ids(_, message):
+    button = InlineKeyboardButton(
+        " ᴄʟᴏsᴇ ",
+        callback_data="close"
+    )
+    markup = InlineKeyboardMarkup([[button]])
+
+    if message.reply_to_message:
+        reply = message.reply_to_message
+
+        if reply.from_user:
+            text = (
+                f"<b><blockquote>"
+                f"👤 ᴜsᴇʀ ɴᴀᴍᴇ: {reply.from_user.first_name}\n"
+                f"🆔 ᴜsᴇʀ ɪᴅ: <code>{reply.from_user.id}</code>\n\n"
+                f"💬 ᴄʜᴀᴛ ɪᴅ: <code>{message.chat.id}</code>"
+                f"</blockquote></b>"
+            )
+        else:
+            text = (
+                f"<b><blockquote>"
+                f"💬 ᴄʜᴀᴛ ɪᴅ: <code>{message.chat.id}</code>\n"
+                f"⚠️ ᴛʜɪs ᴍᴇssᴀɢᴇ ʜᴀs ɴᴏ ᴜsᴇʀ"
+                f"</blockquote></b>"
+            )
+
+        await message.reply_text(
+            text,
+            reply_markup=markup,
         )
+
     else:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
-        markup = InlineKeyboardMarkup([[button]])
-        message.reply(
-           f"ᴛʜɪs ɢʀᴏᴜᴩ's ɪᴅ ɪs: {message.chat.id}",
-           reply_markup=markup
+        text = (
+            f"<b><blockquote>"
+            f"👥 ɢʀᴏᴜᴘ ɴᴀᴍᴇ: {message.chat.title}\n"
+            f"🆔 ɢʀᴏᴜᴘ ɪᴅ: <code>{message.chat.id}</code>\n\n"
+            f"👤 ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ: <code>{message.from_user.id}</code>"
+            f"</blockquote></b>"
+        )
+
+        await message.reply_text(
+            text,
+            reply_markup=markup,
         )
