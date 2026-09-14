2c02837394472492d479cf7d420ed1c4984395af
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 22:20:31 +0530

    Update logger.py

--- BrandrdXMusic/utils/logger.py
+++ BrandrdXMusic/utils/logger.py
@@ -4,11 +4,11 @@ from BrandrdXMusic import app
 from BrandrdXMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
-
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<b><blockquote>
+{app.mention} ᴘʟᴀʏ ʟᴏɢ
 
 <b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
@@ -19,7 +19,10 @@ async def play_logs(message, streamtype):
 <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
 <b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}
+</blockquote></b>
+"""
+
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
@@ -30,4 +33,4 @@ async def play_logs(message, streamtype):
                 )
             except:
                 pass
-        return
+Isse pura **YouTube Play Log** ek single bold blockquote ke andar aayega.
