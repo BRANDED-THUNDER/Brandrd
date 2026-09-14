42b18280086fae5fff47c72161514c4380717e8a
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 20:06:19 +0530

    Update Sangmata.py

--- BrandrdXMusic/plugins/admins/Sangmata.py
+++ BrandrdXMusic/plugins/admins/Sangmata.py
@@ -9,45 +9,70 @@ from BrandrdXMusic import userbot as us, app
 from BrandrdXMusic.core.userbot import assistants
 
 @app.on_message(filters.command("sg"))
-async def sg(client: Client, message: Message):
-    if len(message.text.split()) < 1 and not message.reply_to_message:
-        return await message.reply("sg username/id/reply")
-    if message.reply_to_message:
-        args = message.reply_to_message.from_user.id
-    else:
-        args = message.text.split()[1]
-    lol = await message.reply("<code>Processing...</code>")
-    if args:
-        try:
-            user = await client.get_users(f"{args}")
-        except Exception:
-            return await lol.edit("<code>Please specify a valid user!</code>")
-    bo = ["sangmata_bot", "sangmata_beta_bot"]
-    sg = random.choice(bo)
-    if 1 in assistants:
-        ubot = us.one
-    
-    try:
-        a = await ubot.send_message(sg, f"{user.id}")
-        await a.delete()
-    except Exception as e:
-        return await lol.edit(e)
-    await asyncio.sleep(1)
-    
-    async for stalk in ubot.search_messages(a.chat.id):
-        if stalk.text == None:
-            continue
-        if not stalk:
-            await message.reply("botnya ngambek")
-        elif stalk:
-            await message.reply(f"{stalk.text}")
-            break  
-    
-    try:
-        user_info = await ubot.resolve_peer(sg)
-        await ubot.send(DeleteHistory(peer=user_info, max_id=0, revoke=True))
-    except Exception:
-        pass
-    
+async def sg(client: Client, message: Message): 
+    if len(message.text.split()) < 1 and not message.reply_to_message: 
+        return await message.reply(
+            "<b><blockquote>🔎 sɢ ᴜsᴇʀɴᴀᴍᴇ / ɪᴅ / ʀᴇᴘʟʏ</blockquote></b>",
+        )
+
+    if message.reply_to_message: 
+        args = message.reply_to_message.from_user.id 
+    else: 
+        args = message.text.split()[1] 
+
+    lol = await message.reply(
+        "<b><blockquote>⏳ Pʀᴏᴄᴇssɪɴɢ...</blockquote></b>",
+    )
+
+    if args: 
+        try: 
+            user = await client.get_users(f"{args}") 
+        except Exception: 
+            return await lol.edit(
+                "<b><blockquote>⚠️ Pʟᴇᴀsᴇ sᴘᴇᴄɪғʏ ᴀ ᴠᴀʟɪᴅ ᴜsᴇʀ!</blockquote></b>",
+            )
+
+    bo = ["sangmata_bot", "sangmata_beta_bot"] 
+    sg = random.choice(bo) 
+
+    if 1 in assistants: 
+        ubot = us.one 
+     
+    try: 
+        a = await ubot.send_message(sg, f"{user.id}") 
+        await a.delete() 
+    except Exception as e: 
+        return await lol.edit(
+            f"<b><blockquote>❌ Eʀʀᴏʀ: <code>{e}</code></blockquote></b>",
+        )
+
+    await asyncio.sleep(1) 
+     
+    async for stalk in ubot.search_messages(a.chat.id): 
+        if stalk.text is None: 
+            continue 
+
+        if not stalk: 
+            await message.reply(
+                "<b><blockquote>😤 Bᴏᴛɴʏᴀ ɴɢᴀᴍʙᴇᴋ 😭</blockquote></b>",
+            )
+
+        elif stalk: 
+            await message.reply(
+                f"<b><blockquote>📜 {stalk.text}</blockquote></b>",
+            )
+            break   
+     
+    try: 
+        user_info = await ubot.resolve_peer(sg) 
+        await ubot.send(
+            DeleteHistory(
+                peer=user_info,
+                max_id=0,
+                revoke=True
+            )
+        ) 
+    except Exception: 
+        pass 
+     
     await lol.delete()
-    
