ce48fcefc717122642054e931555b212f24a6cc3
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 13:31:45 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -21,7 +21,9 @@ class WelDatabase:
         if chat_id in self.data:
             del self.data[chat_id]
 
+
 wlcm = WelDatabase()
+
 
 class temp:
     ME = None
@@ -31,88 +33,187 @@ class temp:
     U_NAME = None
     B_NAME = None
 
+
 def circle(pfp, size=(500, 500)):
     pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+
     mask = Image.new("L", bigsize, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + bigsize, fill=255)
+
     mask = mask.resize(pfp.size, Image.LANCZOS)
     mask = ImageChops.darker(mask, pfp.split()[-1])
+
     pfp.putalpha(mask)
     return pfp
 
+
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("BrandrdXMusic/assets/Brandedwel2.png")
+    background = Image.open(
+        "BrandrdXMusic/assets/Brandedwel2.png"
+    )
+
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((825, 824))
+
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('BrandrdXMusic/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('BrandrdXMusic/assets/font.ttf', size=60)
-    draw.text((2100, 1420), f'ID: {id}', fill=(12000, 12000, 12000), font=font)
+
+    font = ImageFont.truetype(
+        "BrandrdXMusic/assets/font.ttf",
+        size=110
+    )
+
+    welcome_font = ImageFont.truetype(
+        "BrandrdXMusic/assets/font.ttf",
+        size=60
+    )
+
+    draw.text(
+        (2100, 1420),
+        f"ID: {id}",
+        fill=(12000, 12000, 12000),
+        font=font
+    )
+
     pfp_position = (1990, 435)
     background.paste(pfp, pfp_position, pfp)
+
     background.save(f"downloads/welcome#{id}.png")
+
     return f"downloads/welcome#{id}.png"
+
 
 @app.on_chat_member_updated(filters.group, group=-3)
 async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
+
     A = await wlcm.find_one(chat_id)
+
     if (
         not member.new_chat_member
-        or member.new_chat_member.status in {"banned", "left", "restricted"}
+        or member.new_chat_member.status in {
+            "banned",
+            "left",
+            "restricted"
+        }
         or member.old_chat_member
     ):
         return
-    user = member.new_chat_member.user if member.new_chat_member else member.from_user
+
+    user = (
+        member.new_chat_member.user
+        if member.new_chat_member
+        else member.from_user
+    )
+
     try:
         pic = await app.download_media(
-            user.photo.big_file_id, file_name=f"pp{user.id}.png"
+            user.photo.big_file_id,
+            file_name=f"pp{user.id}.png"
         )
     except AttributeError:
         pic = "BrandrdXMusic/assets/Brandedwel2.png"
-    if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
+
+    if temp.MELCOW.get(
+        f"welcome-{member.chat.id}"
+    ) is not None:
         try:
-            await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
+            await temp.MELCOW[
+                f"welcome-{member.chat.id}"
+            ].delete()
         except Exception as e:
             LOGGER.error(e)
+
     try:
         welcomeimg = welcomepic(
-            pic, user.first_name, member.chat.title, user.id, user.username
+            pic,
+            user.first_name,
+            member.chat.title,
+            user.id,
+            user.username
         )
-        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
+
+        username = (
+            f"@{user.username}"
+            if user.username
+            else "ɴᴏ ᴜsᴇʀɴᴀᴍᴇ"
+        )
+
+        caption = (
+            f"<b><blockquote>"
+            f"𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗧𝗼 {member.chat.title}\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖\n"
+            f"๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}\n"
+            f"๏ 𝗜𝗗 ➠ <code>{user.id}</code>\n"
+            f"๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ {username}\n"
+            f"๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ "
+            f"<a href=\"https://t.me/BRANDEDKING8\">"
+            f"𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚 📿</a>\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖"
+            f"</blockquote></b>"
+        )
+
+        temp.MELCOW[
+            f"welcome-{member.chat.id}"
+        ] = await app.send_photo(
             member.chat.id,
             photo=welcomeimg,
-            caption=f"""
-𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗧𝗼 {member.chat.title}
-➖➖➖➖➖➖➖➖➖➖➖
-๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
-๏ 𝗜𝗗 ➠ {user.id}
-๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚 📿](https://t.me/BRANDEDKING8)
-➖➖➖➖➖➖➖➖➖➖➖
-""",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/Sustumm_music_bot?startgroup=true")]])
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "⦿ ᴀᴅᴅ ᴍᴇ ⦿",
+                            url=(
+                                "https://t.me/"
+                                "Sustumm_music_bot"
+                                "?startgroup=true"
+                            )
+                        )
+                    ]
+                ]
+            )
         )
+
     except Exception as e:
         LOGGER.error(e)
+
     try:
         os.remove(f"downloads/welcome#{user.id}.png")
         os.remove(f"downloads/pp{user.id}.png")
-    except Exception as e:
+    except Exception:
         pass
 
-@app.on_message(filters.new_chat_members & filters.group, group=-1)
+
+@app.on_message(
+    filters.new_chat_members & filters.group,
+    group=-1
+)
 async def bot_wel(_, message):
     for u in message.new_chat_members:
+
         if u.id == app.me.id:
-            await app.send_message(LOG_CHANNEL_ID, f"""
-NEW GROUP
-➖➖➖➖➖➖➖➖➖➖➖
-𝗡𝗔𝗠𝗘: {message.chat.title}
-𝗜𝗗: {message.chat.id}
-𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄: @{message.chat.username}
-➖➖➖➖➖➖➖➖➖➖➖
-""")
+
+            username = (
+                f"@{message.chat.username}"
+                if message.chat.username
+                else "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
+            )
+
+            log_text = (
+                f"<b><blockquote>"
+                f"📝 𝗡𝗘𝗪 𝗚𝗥𝗢𝗨𝗣\n"
+                f"➖➖➖➖➖➖➖➖➖➖➖\n"
+                f"𝗡𝗔𝗠𝗘: {message.chat.title}\n"
+                f"𝗜𝗗: <code>{message.chat.id}</code>\n"
+                f"𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄: {username}\n"
+                f"➖➖➖➖➖➖➖➖➖➖➖"
+                f"</blockquote></b>"
+            )
+
+            await app.send_message(
+                LOG_CHANNEL_ID,
+                log_text,
+            )
