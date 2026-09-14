23c6e08e6baa21cdbefdbabca40b13ce426f6df3
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 20:09:50 +0530

    Update shayari.py

--- BrandrdXMusic/plugins/admins/shayari.py
+++ BrandrdXMusic/plugins/admins/shayari.py
@@ -96,12 +96,19 @@ SHAYRI = [ " 🌺**बहुत अच्छा लगता है तुझे
 @app.on_message(filters.command(["shayari" ], prefixes=["/", "@", "#"]))
 async def mentionall(client, message):
     chat_id = message.chat.id
+
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply("𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐆𝐫𝐨𝐮𝐩𝐬.")
+        return await message.reply(
+            "<b><blockquote>🚫 𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐆𝐫𝐨𝐮𝐩𝐬.</blockquote></b>",
+        )
 
     is_admin = False
+
     try:
-        participant = await client.get_chat_member(chat_id, message.from_user.id)
+        participant = await client.get_chat_member(
+            chat_id,
+            message.from_user.id
+        )
     except UserNotParticipant:
         is_admin = False
     else:
@@ -110,58 +117,110 @@ async def mentionall(client, message):
             ChatMemberStatus.OWNER
         ):
             is_admin = True
+
     if not is_admin:
-        return await message.reply("𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲, 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 . ")
+        return await message.reply(
+            "<b><blockquote>🚫 𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐧 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲 😏\n\n"
+            "👑 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 𝐔𝐬𝐞 𝐓𝐡𝐢𝐬.</blockquote></b>",
+        )
 
     if message.reply_to_message and message.text:
-        return await message.reply("/shayaril  𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞 ")
+        return await message.reply(
+            "<b><blockquote>⚠️ /shayaril\n\n"
+            "📝 𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞.</blockquote></b>",
+        )
+
     elif message.text:
         mode = "text_on_cmd"
         msg = message.text
+
     elif message.reply_to_message:
         mode = "text_on_reply"
         msg = message.reply_to_message
+
         if not msg:
-            return await message.reply("/shayari  𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞 ...")
+            return await message.reply(
+                "<b><blockquote>⚠️ /shayari\n\n"
+                "📝 𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞...</blockquote></b>",
+            )
+
     else:
-        return await message.reply("/shayari  𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞 ..")
+        return await message.reply(
+            "<b><blockquote>⚠️ /shayari\n\n"
+            "📝 𝐓𝐲𝐩𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 / 𝐑𝐞𝐩𝐥𝐲 𝐀𝐧𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐍𝐞𝐱𝐭 𝐓𝐢𝐦𝐞.</blockquote></b>",
+        )
+
     if chat_id in spam_chats:
-        return await message.reply("𝐏𝐥𝐞𝐚𝐬𝐞 𝐀𝐭 𝐅𝐢𝐫𝐬𝐭 𝐒𝐭𝐨𝐩 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 𝐏𝐫𝐨𝐜𝐞𝐬𝐬 ...")
+        return await message.reply(
+            "<b><blockquote>⏳ 𝐏𝐥𝐞𝐚𝐬𝐞 𝐀𝐭 𝐅𝐢𝐫𝐬𝐭 𝐒𝐭𝐨𝐩 𝐓𝐡𝐞 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 𝐏𝐫𝐨𝐜𝐞𝐬𝐬...</blockquote></b>",
+        )
+
     spam_chats.append(chat_id)
     usrnum = 0
     usrtxt = ""
+
     async for usr in client.get_chat_members(chat_id):
-        if not chat_id in spam_chats:
+        if chat_id not in spam_chats:
             break
+
         if usr.user.is_bot:
             continue
+
         usrnum += 1
-        usrtxt += "<a href='tg://user?id={}'>{}</a>".format(usr.user.id, usr.user.first_name)
+
+        usrtxt += (
+            "<a href='tg://user?id={}'>{}</a>"
+            .format(
+                usr.user.id,
+                usr.user.first_name
+            )
+        )
 
         if usrnum == 1:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt} {random.choice(SHAYRI)}"
-                await client.send_message(chat_id, txt)
+
+                await client.send_message(
+                    chat_id,
+                    txt,
+                    parse_mode=enums.ParseMode.HTML
+                )
+
             elif mode == "text_on_reply":
-                await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
+                await msg.reply(
+                    f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})"
+                )
+
             await asyncio.sleep(4)
             usrnum = 0
             usrtxt = ""
+
     try:
         spam_chats.remove(chat_id)
     except:
         pass
 
 
-#
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-@app.on_message(filters.command(["cancelshayari", "shayarioff"]))
+
+@app.on_message(
+    filters.command(["cancelshayari", "shayarioff"])
+)
 async def cancel_spam(client, message):
-    if not message.chat.id in spam_chats:
-        return await message.reply("𝐂𝐮𝐫𝐫𝐞𝐧𝐭𝐥𝐲 𝐈'𝐦 𝐍𝐨𝐭 ..")
+
+    if message.chat.id not in spam_chats:
+        return await message.reply(
+            "<b><blockquote>⚠️ 𝐂𝐮𝐫𝐫𝐞𝐧𝐭𝐥𝐲 𝐈'𝐦 𝐍𝐨𝐭 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 𝐀𝐧𝐲 𝐒𝐡𝐚𝐲𝐚𝐫𝐢 𝐏𝐫𝐨𝐜𝐞𝐬𝐬.</blockquote></b>",
+        )
+
     is_admin = False
+
     try:
-        participant = await client.get_chat_member(message.chat.id, message.from_user.id)
+        participant = await client.get_chat_member(
+            message.chat.id,
+            message.from_user.id
+        )
     except UserNotParticipant:
         is_admin = False
     else:
@@ -170,11 +229,19 @@ async def cancel_spam(client, message):
             ChatMemberStatus.OWNER
         ):
             is_admin = True
+
     if not is_admin:
-        return await message.reply("𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲, 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 𝐓𝐚𝐠 𝐌𝐞𝐦𝐛𝐞𝐫𝐬.")
+        return await message.reply(
+            "<b><blockquote>🚫 𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐧 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲 😏\n\n"
+            "👑 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 𝐓𝐚𝐠 𝐌𝐞𝐦𝐛𝐞𝐫𝐬.</blockquote></b>",
+        )
+
     else:
         try:
             spam_chats.remove(message.chat.id)
         except:
             pass
-        return await message.reply("👣 𝐁𝐑𝐀𝐍𝐃𝐄𝐃 𝐒𝐇𝐀𝐘𝐀𝐑𝐈 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 💗")
+
+        return await message.reply(
+            "<b><blockquote>🛑 𝐁𝐑𝐀𝐍𝐃𝐄𝐃 𝐒𝐇𝐀𝐘𝐀𝐑𝐈 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 💗</blockquote></b>",
+        )
