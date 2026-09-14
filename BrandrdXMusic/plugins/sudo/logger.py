c38b37f7ab6e8c5c348601995b959c74b4105bf6
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 22:01:04 +0530

    Update logger.py

--- BrandrdXMusic/plugins/sudo/logger.py
+++ BrandrdXMusic/plugins/sudo/logger.py
@@ -8,9 +8,6 @@ from BrandrdXMusic.utils.decorators.language import language
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
 @language
-from pyrogram import enums
-
-
 async def logger(client, message, _):
     usage = _["log_1"]
 
