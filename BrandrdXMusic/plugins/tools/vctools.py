1103d93efd536efef0ed6a0f280f1f3555eb9367
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 13:02:10 +0530

    Update vctools.py

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -100,9 +100,7 @@ async def brah3(client, message: Message):
 
         reply_text = (
             f"<b><blockquote>"
-            f"🔥 {message.from_user.mention} ɪɴᴠɪᴛɪɴɢ:\n\n"
-            f"{users_text}\n\n"
-            f" ᴛᴏ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ\n"
+            f"🔥 {message.from_user.mention} ɪɴᴠɪᴛɪɴɢ: {users_text}:ᴛᴏ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ\n\n"
             f"</blockquote></b>"
         )
 
