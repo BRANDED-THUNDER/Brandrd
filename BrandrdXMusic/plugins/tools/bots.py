ff27c8fd1286782994ebd352f6e84fa3379218fa
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 13:09:40 +0530

    Update bots.py

--- BrandrdXMusic/plugins/tools/bots.py
+++ BrandrdXMusic/plugins/tools/bots.py
@@ -5,25 +5,49 @@ from pyrogram.errors import FloodWait
 
 from BrandrdXMusic import app
 
-
 @app.on_message(filters.command("bots") & filters.group)
 async def bots(client, message):
-
     try:
         botList = []
+
         async for bot in app.get_chat_members(
-            message.chat.id, filter=enums.ChatMembersFilter.BOTS
+            message.chat.id,
+            filter=enums.ChatMembersFilter.BOTS
         ):
             botList.append(bot.user)
+
         lenBotList = len(botList)
-        text3 = f"**ʙᴏᴛ ʟɪsᴛ - {message.chat.title}**\n\n🤖 ʙᴏᴛs\n"
+
+        if not botList:
+            await message.reply_text(
+                "<b><blockquote>🤖 Is group mein koi bot nahi hai.</blockquote></b>",
+                parse_mode=enums.ParseMode.HTML
+            )
+            return
+
+        text3 = (
+            f"<b>ʙᴏᴛ ʟɪsᴛ - {message.chat.title}</b>\n\n"
+            f"<b>🤖 ʙᴏᴛs</b>\n"
+        )
+
         while len(botList) > 1:
             bot = botList.pop(0)
-            text3 += f"├ @{bot.username}\n"
-        else:
-            bot = botList.pop(0)
-            text3 += f"└ @{bot.username}\n\n"
-            text3 += f"**ᴛᴏᴛᴀʟ ɴᴜᴍʙᴇʀ ᴏғ ʙᴏᴛs**: {lenBotList}**"
-            await app.send_message(message.chat.id, text3)
+            username = f"@{bot.username}" if bot.username else bot.first_name
+            text3 += f"├ {username}\n"
+
+        bot = botList.pop(0)
+        username = f"@{bot.username}" if bot.username else bot.first_name
+        text3 += f"└ {username}\n\n"
+
+        text3 += (
+            f"<b>ᴛᴏᴛᴀʟ ɴᴜᴍʙᴇʀ ᴏғ ʙᴏᴛs:</b> "
+            f"<b>{lenBotList}</b>"
+        )
+
+        await app.send_message(
+            message.chat.id,
+            text3,
+        )
+
     except FloodWait as e:
         await asyncio.sleep(e.value)
