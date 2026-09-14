97497de366dc0ead8a30522a117956b038ac18f4
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2026-09-14 22:02:02 +0530

    Update logger.py

--- BrandrdXMusic/plugins/sudo/logger.py
+++ BrandrdXMusic/plugins/sudo/logger.py
@@ -24,7 +24,7 @@ async def logger(client, message, _):
         await message.reply_text(
             f"<b><blockquote>"
             f"🟢 {_[\"log_2\"]}"
-            f"</blockquote></b>",
+            f"</blockquote></b>"
         )
 
     elif state == "disable":
